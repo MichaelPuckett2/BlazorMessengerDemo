8c49cc0b2d55b2a2d44acb3111d5b176262eaf2e
Michael Brian Puckett, II <michael_puckett_ii@live.com>
2021-08-18 15:13:13 -0400

    removed unused usings

--- BlazorServerDemo/Startup.cs
+++ BlazorServerDemo/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BlazorServerDemo.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BlazorServerDemo.Data;
 using Specky.AspNetCore.Extensions;
 
 namespace BlazorServerDemo
