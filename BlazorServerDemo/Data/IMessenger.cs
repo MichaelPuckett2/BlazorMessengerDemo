027e80a04d5ea7765d9d4a7054ecece81e1c98bf
Michael Pucket <michael_puckett_ii@hotmail.com>
2021-08-18 11:24:16 -0400

    added project

---
+++ BlazorServerDemo/Data/IMessenger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorServerDemo.Data
+{
+    public interface IMessenger
+    {
+        List<KeyValuePair<string, string>> Messages { get; set; }
+
+        event EventHandler Updated;
+
+        void SendMessage(string user, string message);
+    }
+}
---
+++ BlazorServerDemo/Data/Messenger.cs
@@ -0,0 +1,19 @@
+﻿using Specky.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace BlazorServerDemo.Data
+{
+    [Speck]
+    public class Messenger : IMessenger
+    {
+        public event EventHandler Updated;
+        public List<KeyValuePair<string, string>> Messages { get; set; } = new();
+
+        public void SendMessage(string user, string message)
+        {
+            Messages.Add(new(user, message));
+            Updated?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
